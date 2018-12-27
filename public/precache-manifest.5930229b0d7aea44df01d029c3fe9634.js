a5f9976dcdf6cd2c6e83a8293f7d61309ad5ca19
Timothy <timothy.keaveny@troyweb.com>
2018-12-27 18:33:46 -0500

    Final build for Heroku deployment

---
+++ public/precache-manifest.5930229b0d7aea44df01d029c3fe9634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e208dfe92d023ed5dddecf9423ea39e",
+    "url": "/static/media/spacex-logo.7e208dfe.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a692d9814eff319c7278",
+    "url": "/static/js/main.a692d981.chunk.js"
+  },
+  {
+    "revision": "74448b3e6e951fad4308",
+    "url": "/static/js/1.74448b3e.chunk.js"
+  },
+  {
+    "revision": "a692d9814eff319c7278",
+    "url": "/static/css/main.4657f092.chunk.css"
+  },
+  {
+    "revision": "dc8bf8a5fa933fac52e03fac55d76401",
+    "url": "/index.html"
+  }
+];
