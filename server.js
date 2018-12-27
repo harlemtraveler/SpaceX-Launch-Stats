ab64403b01e6380dd55e722bb3654cc5bd2fab58
Timothy <timothy.keaveny@troyweb.com>
2018-12-27 18:25:44 -0500

    Make all paths other than "/graphql" point to React public folder (after "build")

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
+
+// Set a static folder
+app.use(express.static('public'));
+
+// All other paths lead to React's "index.html" file in public dir
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, public, 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000
 
